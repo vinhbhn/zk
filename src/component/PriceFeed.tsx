13db033d7df112a9e2aa12864a6445e0431c38d5
vinhbhn <bvinhhn@gmail.com>
2021-03-09 15:08:42 +0700

    add env file

--- src/component/PriceFeed.tsx
+++ src/component/PriceFeed.tsx
@@ -1,61 +1,62 @@
-import Web3 from "web3";
+import Web3 from 'web3';
+import 'dotenv/config';
 
 const web3 = new Web3(
-  "https://mainnet.infura.io/v3/713ad71148b74747b97a4ce5ce51112f"
+  `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`
 );
 
 const aggregatorV3InterfaceABI: any[] = [
   {
     inputs: [],
-    name: "decimals",
-    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
-    stateMutability: "view",
-    type: "function",
+    name: 'decimals',
+    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     inputs: [],
-    name: "description",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
+    name: 'description',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
   },
   {
-    inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
-    name: "getRoundData",
+    inputs: [{ internalType: 'uint80', name: '_roundId', type: 'uint80' }],
+    name: 'getRoundData',
     outputs: [
-      { internalType: "uint80", name: "roundId", type: "uint80" },
-      { internalType: "int256", name: "answer", type: "int256" },
-      { internalType: "uint256", name: "startedAt", type: "uint256" },
-      { internalType: "uint256", name: "updatedAt", type: "uint256" },
-      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+      { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+      { internalType: 'int256', name: 'answer', type: 'int256' },
+      { internalType: 'uint256', name: 'startedAt', type: 'uint256' },
+      { internalType: 'uint256', name: 'updatedAt', type: 'uint256' },
+      { internalType: 'uint80', name: 'answeredInRound', type: 'uint80' },
     ],
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     inputs: [],
-    name: "latestRoundData",
+    name: 'latestRoundData',
     outputs: [
-      { internalType: "uint80", name: "roundId", type: "uint80" },
-      { internalType: "int256", name: "answer", type: "int256" },
-      { internalType: "uint256", name: "startedAt", type: "uint256" },
-      { internalType: "uint256", name: "updatedAt", type: "uint256" },
-      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+      { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+      { internalType: 'int256', name: 'answer', type: 'int256' },
+      { internalType: 'uint256', name: 'startedAt', type: 'uint256' },
+      { internalType: 'uint256', name: 'updatedAt', type: 'uint256' },
+      { internalType: 'uint80', name: 'answeredInRound', type: 'uint80' },
     ],
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     inputs: [],
-    name: "version",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
+    name: 'version',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
   },
 ];
 
-export const ethAddr: string = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
-export const linkAddr: string = "0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c";
+export const ethAddr: string = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
+export const linkAddr: string = '0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c';
 
 export const checkPriceFeed = (tokenAddr: string) => {
   return new web3.eth.Contract(aggregatorV3InterfaceABI, tokenAddr);
